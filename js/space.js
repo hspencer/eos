8b372d258bb86ca6951d1a7621ae66c42a99af4d
Herbert Spencer <herbert.spencer@gmail.com>
2016-08-28 19:20:35 -0300

    adjustments

--- js/space.js
+++ js/space.js
@@ -1,6 +1,9 @@
 var rx = 0; 
 var ry = 0;
-
+var dirX = 0;
+var dirY = 0;
+var sc = 20;
+new p5.RendererGL()
 
 function setup() {
 	spaceCanvas = createCanvas(200, 200, WEBGL);
@@ -10,25 +13,105 @@ function setup() {
 
 function draw() {
 	stroke(0);
-	noFill();
-	
+	fill(206, 186, 0);
 	push();
-	scale(20);
 
-	if(mouseIsPressed &&mouseX > 0 && mouseX < width &&
+	// mouse
+	if(mouseIsPressed  && mouseX > 0 && mouseX < width &&
 	   mouseY > 0 && mouseY < height){
-	   	rx = mouseY/height;
-		ry = mouseX/width;	
+	   	
+	   	rx = mouseY / height * PI;
+		ry = mouseX / width * PI;	
+
+		dirX = (mouseX / width - 0.5) *2;
+  		dirY = (mouseY / height - 0.5) *(-2);
+	}
+
+	// touch
+	if(touchIsDown && touchX > 0 && touchX < width &&
+	   touchY > 0 && touchY < height){
+	   	
+	   	rx = touchY / height * PI;
+		ry = touchX / width * PI;	
+
+		dirX = (touchX / width - 0.5) *2;
+  		dirY = (touchY / height - 0.5) *(-2);	
 	}
 
 	rotateX(rx);
 	rotateY(ry);
 
-	box(document.getElementById('sx').value, 
-		document.getElementById('sy').value,
-		document.getElementById('sz').value);
+	directionalLight(206, 186, 10, dirX, dirY, 0.25);
+
+	wireBox(document.getElementById('sx').value * sc, 
+		document.getElementById('sy').value * sc,
+		document.getElementById('sz').value * sc);
+	
 	pop();
 }
+
+function wireBox(xdim, ydim, zdim){
+	var t = 2; // beam thickness
+	
+	// top left - along in z
+	
+	push();
+		translate(-xdim/2, -ydim/2, 0);
+		box(t, t, zdim);
+	pop();
+
+
+	// top right - along in z
+
+	push();
+		translate(xdim/2, -ydim/2, 0);
+		box(t, t, zdim);
+	pop();
+
+	// floor
+	
+	push();
+		translate(0, ydim/2, 0);
+		box(xdim, t, zdim);
+	pop();
+
+	// top front
+
+	push();
+		translate(0, -ydim/2, zdim/2);
+		box(xdim, t, t);
+	pop();
+
+	// top back
+
+	push();
+		translate(0, -ydim/2, -zdim/2);
+		box(xdim, t, t);
+	pop();
+
+	// poles
+
+	push();
+		translate(-xdim/2, 0, -zdim/2);
+		box(t, ydim, t);
+	pop();
+
+	push();
+		translate(xdim/2, 0, -zdim/2);
+		box(t, ydim, t);
+	pop();
+
+	push();
+		translate(-xdim/2, 0, zdim/2);
+		box(t, ydim, t);
+	pop();
+
+	push();
+		translate(xdim/2, 0, zdim/2);
+		box(t, ydim, t);
+	pop();
+}
+
 
 function adjustCanvasSize(){
   	var w = document.getElementById('space').offsetWidth;
